45e8a22141e58e67696fac10bdbe9461413243e3
Po-Hsun Chang <pohsun@umich.edu>
2025-04-16 01:42:01 -0400

    for new data

--- Examples/MBot_examples.sh
+++ Examples/MBot_examples.sh
@@ -3,7 +3,19 @@
 pathDatasetMBot='/Datasets/MBot'
 
 echo "Launching MBot with Monocular sensor"
-./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/MBot.yaml "$pathDatasetMBot"/maze_0415_FRB_2 ./Monocular/MBot_TimeStamps/maze_0415_FRB_2.txt dataset-MBot_mono
+./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/MBot.yaml "$pathDatasetMBot"/maze_0415_FRB_6 ./Monocular/MBot_TimeStamps/maze_0415_FRB_6.txt dataset-MBot_mono
+
+# echo "Launching MBot with Monocular sensor"
+# ./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/MBot.yaml "$pathDatasetMBot"/maze_0415_FRB_5 ./Monocular/MBot_TimeStamps/maze_0415_FRB_5.txt dataset-MBot_mono
+
+# echo "Launching MBot with Monocular sensor"
+# ./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/MBot.yaml "$pathDatasetMBot"/maze_0415_FRB_4 ./Monocular/MBot_TimeStamps/maze_0415_FRB_4.txt dataset-MBot_mono
+
+# echo "Launching MBot with Monocular sensor"
+# ./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/MBot.yaml "$pathDatasetMBot"/maze_0415_FRB_3 ./Monocular/MBot_TimeStamps/maze_0415_FRB_3.txt dataset-MBot_mono
+
+# echo "Launching MBot with Monocular sensor"
+# ./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/MBot.yaml "$pathDatasetMBot"/maze_0415_FRB_2 ./Monocular/MBot_TimeStamps/maze_0415_FRB_2.txt dataset-MBot_mono
 
 # echo "Launching MBot with Monocular sensor"
 # ./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/MBot.yaml "$pathDatasetMBot"/maze_0415_FRB ./Monocular/MBot_TimeStamps/maze_0415_FRB.txt dataset-MBot_mono
