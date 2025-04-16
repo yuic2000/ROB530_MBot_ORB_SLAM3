0695dc05c5215dbed1e5bf874b1d1e721a3d23aa
yuic <yuic@umich.edu>
2025-04-15 23:25:06 -0400

    add yaml, dataset and bash file for MBot

---
+++ Examples/MBot_examples.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pathDatasetMBot='/Datasets/MBot'
+
+echo "Launching MBot with Monocular sensor"
+./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/MBot.yaml "$pathDatasetMBot"/maze_0415_FRB_2 ./Monocular/MBot_TimeStamps/maze_0415_FRB_2.txt dataset-MBot_mono
+
+# echo "Launching MBot with Monocular sensor"
+# ./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/MBot.yaml "$pathDatasetMBot"/maze_0415_FRB ./Monocular/MBot_TimeStamps/maze_0415_FRB.txt dataset-MBot_mono
+
+# echo "Launching MBot with Monocular sensor"
+# ./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/MBot.yaml "$pathDatasetMBot"/square_0410_FRB ./Monocular/MBot_TimeStamps/square_0410_FRB.txt dataset-MBot_mono
+
+# echo "Launching MBot with Monocular sensor"
+# ./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/MBot.yaml "$pathDatasetMBot"/square_0410_home ./Monocular/MBot_TimeStamps/square_0410_home.txt dataset-MBot_mono
+
+# echo "Launching MBot with Monocular sensor"
+# ./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/MBot.yaml "$pathDatasetMBot"/square_0410_home_kernel ./Monocular/MBot_TimeStamps/square_0410_home.txt dataset-MBot_mono
